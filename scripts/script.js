bf2d93825748110088d24847645057f1307cada5
Hachatur Khachatryan <hachatur4@yandex.ru>
2023-02-18 19:19:26 +1000

    Исправления ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,30 @@
 
 let popupOpen = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
+let buttonEdit = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close-icon');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let popupContainer = document.querySelector('.popup__container');
-let nameInput = popupContainer.querySelector('.popup__input-name');
-let jobInput = popupContainer.querySelector('.popup__input-job');
-let submitButton = popupContainer.querySelector('.popup__submit-button');
+let formProfile = document.querySelector('.form-profile');
+let nameInput = formProfile.querySelector('#popup-name');
+let jobInput = formProfile.querySelector('#popup-job');
 
-function formOpen ( ) {
-  popupOpen.classList.add('popup_opened')
+function openForm ( ) {
+  popupOpen.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function handleClosingSubmit ( ) {
   popupOpen.classList.remove('popup_opened');
-  nameInput.value = '';
-  jobInput.value = '';
 }
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupOpen.classList.remove('popup_opened');
+  handleClosingSubmit ();
 }
 
-submitButton.addEventListener('click', handleFormSubmit);
+buttonEdit.addEventListener('click', openForm);
+formProfile.addEventListener('submit', handleFormSubmit);
 popupClose.addEventListener('click', handleClosingSubmit);
-editButton.addEventListener('click', formOpen);
